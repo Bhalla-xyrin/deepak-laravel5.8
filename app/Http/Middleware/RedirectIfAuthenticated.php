6869a891c28e2ebd85f249edd27c50874089e946
deepak <deepakbhalla52@gmail.com>
2019-09-04 00:04:02 +0530

    redirect controller

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,6 +17,12 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
+        if ($guard == "admin" && Auth::guard($guard)->check()) {
+            return redirect('/admin');
+        }
+        if ($guard == "author" && Auth::guard($guard)->check()) {
+            return redirect('/author');
+        }
         if (Auth::guard($guard)->check()) {
             return redirect('/home');
         }
